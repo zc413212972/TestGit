63083e8194d4af5009fe52efbda1412562956a98
LiuKeYao <liukeyao123@126.com>
2018-06-27 22:09:12 +0800

    多加了helloworld1

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -2,6 +2,9 @@ package test;
 
 public class Test2 {
     public static void main(String[] args) {
+
         System.out.println("hello world");
+        System.out.println("hello world1");
+
     }
 }
